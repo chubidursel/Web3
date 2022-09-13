0e60e0928493654fcae6de8d1dd791eae62d4e12
Alexpn <alexpvn69@gmail.com>
2022-09-14 00:34:31 +0300

    first commit

--- src/pages/Token/ERC20.tsx
+++ src/pages/Token/ERC20.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import defaultProvider from '../../abi/defaultProvider';
 import { contractERC20, contractERC20WithSigner } from '../../components/smart_contract/erc20';
 import {useState, useContext} from 'react';
@@ -7,12 +6,12 @@ import Header from '../../components/header';
 import { Context } from "../../contexts/context";
 import walletProvider from '../../abi/walletProvider';
 import {Info} from "./ERC20_components/info"
+import HeaderToken from './ERC20_components/headerToken';
 
 export function ERC20() {
   const [owner, setOwner] = useState()
   const [add, setAdd] = useState("")
   const [balance, setBalance] = useState()
-
   const [addressTo, setAddressTo] = useState("");
   const [amountTo, setAmountTo] = useState();
 
@@ -52,43 +51,49 @@ export function ERC20() {
   return (
     <>
     <Header />
-<div className="py-6 flex flex-col justify-center sm:py-12 ">
+<div className="py-6 flex w- auto justify-center text-purple-800">
 <div className="py-3 sm:max-w-xl sm:mx-auto ">
-<div className="bg-white min-w-1xl flex flex-col rounded-xl shadow-lg m-2">
-      <div className="px-12 py-5 ">
+{/* <div className="bg-white min-w-1xl flex flex-col rounded-xl shadow-lg m-2"> */}
+      
+      
+      {/* <div className="px-12 py-5 ">
         <h2 className="text-purple-800 text-3xl text-center font-semibold">Check out our ERC20 token</h2>
       </div>
+
+
+
       <div className="bg-gray-200 w-full flex flex-col items-center">
+
+
       <div className="flex flex-row space-x-4 items-center py-6 space-y-3">
           <a href='https://etherscan.io/address/0x7C2ED4E6fB642186ec9472813207c902005583D7' target="_blank"><span className="text-gray-800 text-3xl font-semibold hover:underline">Etherscan </span></a>
           <Link to="/Defi/exchange" ><span className="text-gray-800 text-3xl font-semibold hover:underline">Exchange</span> </Link>
-      </div>
-
+      </div> */}
+<HeaderToken />
       <Info />
 
-      <div className='p-5 bg-orange-100'>
-        <h1 className='text-4xl text-center'>Token Function:</h1>
-        <div className="bg-pink-400 p-2">
-          <label >Check your balance: 
-            <input onChange={(event)=>setAdd(event.target.value)}></input>
-            <button onClick={hadleSupply} className="bg-orange-600 px-10 rounded-xl">submit</button>
-            <h1 className='text-center font-bold'>{balance}</h1>
+      <div className='bg-blue-100 rounded-2xl border-4 border-red-400 w-full p-3 m-3 text-xl'>
+        <h1 className=" text-3xl text-center font-bold m-3">Token function</h1>
+        {/* <div className="bg-pink-400 p-2"> */}
+          <label>Check your balance: 
+            <input placeholder='Enter your address' onChange={(event)=>setAdd(event.target.value)} className = 'ml-3 rounded'/>
+            <button onClick={hadleSupply} className="font-bold ml-3 rounded-2xl border-2 border-red-400 px-[15px] hover:bg-red-400">Submit</button>
+            <h1 >{balance}</h1>
           </label>
-        </div>
-        
-        <form onSubmit={handleTransaction} className="bg-green-400 p-2">
-          <h1 className='text-center font-bold underline'>Transfer token</h1>
-          <label>send to: </label>
-          <input onChange={(e)=>setAddressTo(e.target.value)} className='rounded'></input><br />
-          <label>amount:</label>
-          <input type='text' onChange={(e:any)=>setAmountTo(e.target.value)}></input>
-          <button type="submit" className="ml-10 px-2 bg-orange-500" >click</button>
+        {/* </div> */}
+        <form onSubmit={handleTransaction}>
+          <h1 className='text-center font-bold'>Transfer token</h1>
+          <label>Send to: </label>
+          <input onChange={(e)=>setAddressTo(e.target.value)} className='rounded' placeholder='Enter address of reciever'></input><br />
+          <label>Amount:</label>
+          <input type='text' className='rounded' onChange={(e:any)=>setAmountTo(e.target.value)} placeholder='Enter amount of tokens' />
+          <button type="submit" className="font-bold ml-3 rounded-2xl border-2 border-red-400 px-[15px] hover:bg-red-400">Transfer</button>
         </form>
       </div>
 
       </div>
+{/* </div> */}
 </div>
-</div>
-</div>
+
 </>
   )}
--- src/pages/Token/ERC20_components/info.tsx
+++ src/pages/Token/ERC20_components/info.tsx
@@ -3,20 +3,21 @@ import React from 'react'
 export function Info() {
   return (
     <>
-    <div className='bg-blue-100 w-full py-5 px-2'>
-        <h1 className='font-bold text-center'>INFO</h1>
-
-        <label htmlFor="chain">Network:</label>
+    <div className='bg-blue-100 rounded-2xl border-4 border-red-400 w-full p-3 m-3'>
+        <h1 className=" text-3xl text-center font-bold m-3">INFO</h1>
+<div className='text-xl'>
+        <label htmlFor="chain" className='mr-3 font-bold'>Network:</label>
             <select name="chain" id="chain" form="carform">
             <option value="ETH">ETH</option>
             <option value="Poligon">Poligon</option>
             <option value="BNB">BNB</option>
-            </select>
-            <p>Token name: CryptoWorldToken</p>
-        <p>Token address: 0x7C2ED4E6fB642186ec9472813207c902005583D7 </p>
-        <p>Owner: 0x98162D17D4d15c945B7418475EdEb4d9c0335684</p>
-        <p>total supply: 1000</p>
-    </div>
+            </select>         
+            <p className='font-bold'>Token name:</p> <p>CryptoWorldToken</p>
+            <p className='font-bold'>Token address:</p> <p>0x7C2ED4E6fB642186ec9472813207c902005583D7</p>
+            <p className='font-bold'>Owner:</p> <p>0x98162D17D4d15c945B7418475EdEb4d9c0335684</p>
+            <p className='font-bold'>Total supply:</p> <p>1000</p>
+            
+    </div></div>
     </>
   )
 }
