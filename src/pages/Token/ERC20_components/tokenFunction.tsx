8d6e646c5ca7072361c76fa05f9900798772da74
chubidursel <baurimk21@gmail.com>
2022-09-17 21:26:20 +0700

    refresh after merge

--- src/pages/Token/ERC20_components/tokenFunction.tsx
+++ src/pages/Token/ERC20_components/tokenFunction.tsx
@@ -24,22 +24,10 @@ export default function TokenFunction() {
 
   const [amountApprove, setAmountApprove] = useState();
   const [addressToArrpove, setAddressToApporve] = useState("");
-  // const[result, setResult] = useState(); //result???
 
-
-//<<<<<<<<<<<<<<<<<<<<<  TESTING
 const { contextState, updateContextState } = useAppContext();
 const currentAccount = contextState?.currentAccount;
 
-  // async function getData(){
-  //   const blockNum = await defaultProvider.getBlockNumber();
-  //   console.log(walletProvider)
-
-  //   console.log(currentAccount)
-
-  // }
-
-//<<<<<<<<<<<<<<<<<<<<
   const hadleSupply = async()=>{
     try {
       setLoaderBal(true)
@@ -109,8 +97,8 @@ const currentAccount = contextState?.currentAccount;
       <div className='bg-blue-100 rounded-2xl border-4 border-red-400 text-xl px-[15px]'>
         <h1 className=" text-3xl text-center font-bold m-3">Token function</h1>
    
-       <div className='rounded-2xl border-2 border-red-400 px-[15px] p-2 m-2'>
-          <h1 className='text-center font-bold'>Check your balance</h1>
+       <div className='rounded-2xl border-2 border-red-400 px-[15px] p-2 m-2 hover:bg-blue-200 hover:shadow-xl'>
+          <h1 className='text-center font-bold mb-1'>Check your balance</h1>
             <input placeholder='Enter your address' onChange={(event)=>setAdd(event.target.value)} className = 'rounded' />
             
             {loaderBal ? 
@@ -120,13 +108,13 @@ const currentAccount = contextState?.currentAccount;
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
               </svg>
               <span class="sr-only">Loading...</span>
-            </div> : <button onClick={hadleSupply} className="font-bold ml-3 rounded-2xl border-2 border-red-400 px-[15px] hover:bg-red-400">Submit</button>}
+            </div> : <button onClick={hadleSupply} className="font-bold ml-3 rounded-2xl border-2 border-red-400 px-[15px] hover:bg-red-400">Check</button>}
             
             {balance ? <h1 className='font-bold'>Your balance: {balance} CWT</h1> 
                       : (errorBal ? <h1 className='font-bold text-red-500 text-2xl '>{errorBal}</h1> : null) }
         </div>
 
-        <div className='rounded-2xl border-2 border-red-400 px-[15px] p-2 m-2'>
+        <div className='rounded-2xl border-2 border-red-400 px-[15px] p-2 m-2 hover:bg-blue-200 hover:shadow-xl'>
         <form onSubmit={handleTransaction}>
           <h1 className='text-center font-bold p-1'>Transfer token</h1>
           <label>Send to:</label>
@@ -149,11 +137,11 @@ const currentAccount = contextState?.currentAccount;
 
 
 
-<div className='rounded-2xl border-2 border-red-400 px-[15px] p-2 m-2'>
+<div className='rounded-2xl border-2 border-red-400 px-[15px] p-2 m-2 hover:bg-blue-200 hover:shadow-xl'>
         <form onSubmit={handleApprove}>
           <h1 className='text-center font-bold'>Approve</h1>
           <label>Approve:</label>
-          <input onChange={(e)=>setAddressToApporve(e.target.value)} className='rounded ml-3' placeholder='Enter address of reciever' /><br />
+          <input onChange={(e)=>setAddressToApporve(e.target.value)} className='rounded ml-3 mb-1' placeholder='Enter address of reciever' /><br />
           <label>Amount:</label>
           <input type='text' className='rounded ml-3' onChange={(e:any)=>setAmountApprove(e.target.value)} placeholder='Enter amount of tokens' />
         
