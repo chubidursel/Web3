57a718256203a10d97460a968bfb22ceaca3871e
chubidursel <baurimk21@gmail.com>
2022-09-29 00:25:12 +0700

    small bugs

--- src/components/Other/Game.tsx
+++ src/components/Other/Game.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../headerNew'
-import Sidebar from '../Sidebar'
+
 
 function Game() {
   return (<>
--- src/components/Other/Note.tsx
+++ src/components/Other/Note.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import Header from '../headerNew'
-import Sidebar from '../Sidebar'
 
 function Note() {
   return (
     <>
     <Header >FCK TS</Header>
-
-  
     <div>
         <h1>Write your note: </h1>
         <h1>IPFS</h1>
