5f3b782b0c412e0eb6df881088cb6a1e2788043c
chubidursel <baurimk21@gmail.com>
2023-01-10 13:35:00 -0800

    new SC nft

--- Ethereum/contracts/NFT.sol
+++ Ethereum/contracts/NFT.sol
@@ -1,38 +1,48 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
+/*
+   ______  ______               ______     ______  
+ .' ___  ||_   _ `.           .' ____ \  .' ___  | 
+/ .'   \_|  | | `. \  ______  | (___ \_|/ .'   \_| 
+| |         | |  | | |______|  _.____`. | |        
+\ `.___.'\ _| |_.' /          | \____) |\ `.___.'\ 
+ `.____ .'|______.'            \______.' `.____ .' 
+   ChubiDuracell                 smart contract
+*/
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract CircleArt is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract HowToFix is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
 
     using Counters for Counters.Counter;
     using Strings for uint256;
 
-    uint8 public maxSupply = 33;
+    uint public maxSupply = 500;
     uint8 public maxPerWallet = 3;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("CircleArt", "CA") {
+    constructor() ERC721("HowToFix", "HTF") {
         _tokenIdCounter.increment();
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmXJ3DPJ5upfYMtJCS4yttqbbhYaJprHFXoLZNF3kqth3Z/";  
+        return "ipfs://QmRB4k8jQNeTRUHQZEvStPzSGBwAJWhqvfYUVHCq9GsKVR/";  
     }
-
 
     function safeMint(address to) public onlyOwner {
         require(_tokenIdCounter.current() <= maxSupply, "Opps, all NFT has been minted");
 
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI(tokenId));
     }
@@ -80,6 +90,31 @@ contract CircleArt is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         _to.transfer(getBalanceEth());
     }
 
-    //WHITELIST??
-    //ADD MORE FEAURES
+    //------------------ Enumerable > to get all arr of tokens by addr -----------------------
+    // MyFunc
+    function tokenList(address _who) public view returns(uint[] memory){
+        uint amountTokens =  balanceOf(_who);
+        uint[] memory tempArray = new uint[](amountTokens);
+
+        for(uint i = 0; i < amountTokens; i++) {
+        tempArray[i] = tokenOfOwnerByIndex(_who, i);
+        }
+        return tempArray;
+    }
+    //requried by sol
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
